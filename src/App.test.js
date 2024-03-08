fd492ebc18c72237be7a4b6fb9bcdb36de87ab97
santiag0Forer0 <160348135+santiag0Forer0@users.noreply.github.com>
2024-03-08 22:35:46 +0000

    Hola

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-
+<h1>Hola</h1>
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
