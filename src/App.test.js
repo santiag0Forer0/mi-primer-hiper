d200cdc800fa8d45571fce208a45ab12181e66c6
santiag0Forer0 <160348135+santiag0Forer0@users.noreply.github.com>
2024-03-15 16:54:45 +0000

    a

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-<p>la pagina no se actualiza</p>
+<p>la pagina no se actualizaba</p>
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
