47f4294892718caee1b304d501365c881502652c
santiag0Forer0 <160348135+santiag0Forer0@users.noreply.github.com>
2024-03-15 17:42:36 +0000

    e

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h1> Hola</h1>
+      <h1> Hola </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
