a0a7c5ebac205d297b07b20ce13ab602973db418
santiag0Forer0 <160348135+santiag0Forer0@users.noreply.github.com>
2024-03-15 17:35:09 +0000

    act

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src= " " />
+        <H1> actualizaba</H1>
+        <img src= "https://static.wikia.nocookie.net/heroe/images/3/34/Doom_Slayer_DE_render_1.png/revision/latest?cb=20210720002059&path-prefix=es " />
         <p>
           Hola
         </p>
