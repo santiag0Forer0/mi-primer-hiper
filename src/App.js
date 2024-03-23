1ee4e5ba7395db9d2fe9a2ab9c49bcb5723bd2dd
santiag0Forer0 <160348135+santiag0Forer0@users.noreply.github.com>
2024-03-23 01:41:52 +0000

    b

--- src/App.js
+++ src/App.js
@@ -5,15 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h1> <p>Hola</p> </h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1> Hola </h1>
+
       </header>
     </div>
   );
