433d6269dec6a91822b4369a5b99cb806c894db8
santiag0Forer0 <160348135+santiag0Forer0@users.noreply.github.com>
2024-03-25 18:22:35 +0000

    pag

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <h1> Buenos dias </h1>
       <h2>Una imagen</h2>
       <p>La pagina no se esta actualizando, no se si es el programa o estoy haciendo algo mal</p>
-       <img src='https://i.pinimg.com/564x/d3/32/be/d332be98631df8e63ff06dc6755e5cdf.jpg'></img>
+       <img src="https://i.pinimg.com/564x/d3/32/be/d332be98631df8e63ff06dc6755e5cdf.jpg"> </img>
        <p>La imagen de un bicho</p>
        </>
   );
