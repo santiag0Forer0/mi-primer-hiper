19339051cb94ec2b12f1bed525d186f8e1c967b8
santiag0Forer0 <160348135+santiag0Forer0@users.noreply.github.com>
2024-03-25 18:15:45 +0000

    bicho

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h1> Hola </h1>
+      <h1> Buenos dias </h1>
+      <h2>Una imagen</h2>
       <p>La pagina no se esta actualizando, no se si es el programa o estoy haciendo algo mal</p>
        <img src='https://i.pinimg.com/564x/d3/32/be/d332be98631df8e63ff06dc6755e5cdf.jpg'></img>
+       <p>La imagen de un bicho</p>
         <a
           className="App-link"
           href="https://reactjs.org"
