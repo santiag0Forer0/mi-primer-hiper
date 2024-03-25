467b4cafe38ad059862099cfb8d9b45405c3147a
santiag0Forer0 <160348135+santiag0Forer0@users.noreply.github.com>
2024-03-25 17:36:03 +0000

    f

--- src/App.js
+++ src/App.js
@@ -3,7 +3,19 @@ import './App.css';
 
 function App() {
   return (
-
+    <div className="App">
+      <header className="App-header">
+      <h1> Hola </h1>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
