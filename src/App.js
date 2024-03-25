dc7ae04aa5cd2b4cec3374f92a015f5f2d7d8cab
santiag0Forer0 <160348135+santiag0Forer0@users.noreply.github.com>
2024-03-25 17:59:27 +0000

    a

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
       <h1> Hola </h1>
+      <p>La pagina no se esta actualizando, no se si es el programa o estoy haciendo algo mal</p>
+       <img src='https://i.pinimg.com/564x/d3/32/be/d332be98631df8e63ff06dc6755e5cdf.jpg'></img>
         <a
           className="App-link"
           href="https://reactjs.org"
